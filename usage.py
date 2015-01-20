c8d605c4fc7448d7e471652da63cff43f6ad4225
Darren Boss <darren.boss@duke.edu>
2015-01-20 17:30:44 -0500

    Template is now working after appending the dict to a list

--- usage.py
+++ usage.py
@@ -1,20 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
 import os
 import subprocess
 
 app = Flask(__name__)
 
-def partition_usage(n):
-    df = subprocess.Popen(["df", n], stdout=subprocess.PIPE)
+def partition_usage(filesystem):
+    df = subprocess.Popen(["df", filesystem], stdout=subprocess.PIPE)
     output = df.communicate()[0]
+    lines = output.split('\n')
+    fs, size, used, available, percent, mountpoint = lines[1].split()
+    #output_dict = dict(item.split() for item in output.split("\n"))
     #device, size, userid, available, percent, mountpoint = \
     #        output.split("\n")[1].split()
-    return output 
+    #return output_dict
+    return dict(name=fs, total=size, used=used, free=available, percent=percent)
 
 @app.route('/')
 def hello_world():
-    return partition_usage('/media/itlab')
-    #return 'Hello World!'
+    usage = partition_usage('/media/itlab')
+    print usage
+    usage_list = []
+    usage_list.append(usage)
+    return render_template('usage.html', mounts=usage_list)
 
 if __name__ == '__main__':
     app.debug = True
